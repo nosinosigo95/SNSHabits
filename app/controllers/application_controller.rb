40a0897ba5525c79a633897428bf92d880361412
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-03 22:08:17 +0900

    iconをUserモデルに紐付ける

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  protected
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
 end
