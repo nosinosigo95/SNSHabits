e45102b4a22bcedc4d501c3be93733dd14245216
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-03 10:07:04 +0900

    名前とメールアドレス、パスワードの存在性のテストを追加する

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     name {"guest"}
     email {"sample@example.com"}
-    password {"guest"}
+    password {"123456"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "名前とメールアドレス、パスワードがあれば、テストが有効になること" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+  it "名前がなければ、テストが無効になること" do
+    expect(FactoryBot.build(:user, name: nil)).to be_invalid
+  end
+  it "名前がスペースだけならば、テストが無効になること" do
+    expect(FactoryBot.build(:user, name: "   ")).to be_invalid
+  end
+  it "メールアドレスがなければ、テストが無効になること" do
+    expect(FactoryBot.build(:user, email: nil)).to be_invalid
+  end
+  it "メールアドレスがスペースだけならば、テストが無効になること" do
+    expect(FactoryBot.build(:user, email: "    ")).to be_invalid
+  end
+  it "パスワードがなければ、テストが無効になること" do
+    expect(FactoryBot.build(:user, password: nil)).to be_invalid
+  end
+  it "パスワードがスペースだけならば、無効になること" do
+    expect(FactoryBot.build(:user, password: "  ")).to be_invalid
+  end
 end
