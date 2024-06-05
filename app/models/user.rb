51794b948005d4f055cb9ce84a6734484e7bf61f
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-05 15:33:20 +0900

    自己紹介に文字数制限を加える

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
+  INTRO_MAX = 400
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable#, :confirmable
   validates :name, presence: true, uniqueness: true
+  validates :introduction, length: {maximum: INTRO_MAX}
   has_one_attached :icon
 end
