54699836f17950c6a37fe2a48ccbc02a0a2d4039
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-05 15:27:24 +0900

    ユーザー名を一意にする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable#, :confirmable
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   has_one_attached :icon
 end
