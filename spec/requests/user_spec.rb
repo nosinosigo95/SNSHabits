64ccf9dc4a233af4d1a94b22cfbfd77b89d6f80f
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-05 17:36:15 +0900

    アカウントページのリクエストスペックを追加する

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/user/show"
-      expect(response).to have_http_status(:success)
-    end
+  let(:user){FactoryBot.create(:user)}
+  
+  before do
+    sign_in user
+    get "/user/show"
   end
 
+  describe "アカウントページ(/user/show)" do
+    it "正常にアカウントページにアクセスできること" do
+      expect(response).to have_http_status(:success)
+    end
+    it "ユーザーの名前がレスポンスにあること" do
+      expect(response.body).to include(user.name)
+    end
+    it "ユーザーのメールアドレスがレスポンスにあること" do
+      expect(response.body).to include(user.email)
+    end
+  end
 end
