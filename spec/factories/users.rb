549a06c42606fd6912712aaa489b475c428bbcb0
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-05 18:46:12 +0900

    アイコンのテストを追加する

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,8 @@ FactoryBot.define do
     name {"guest"}
     email {"sample@example.com"}
     password {"123456"}
+    after(:build) do |user|
+       user.icon.attach(io: File.open('spec/fixtures/image/person-circle.svg'), filename: 'person-circle.svg', content_type: 'image/svg')
+     end
   end
 end
