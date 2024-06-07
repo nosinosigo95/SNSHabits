ebf3b2f7153143dd42b668cd7fb5e1c9c5e3b226
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-07 17:31:35 +0900

    習慣のモデルスペックのテスト項目を書く

--- app/models/good_habit.rb
+++ app/models/good_habit.rb
@@ -1,3 +1,3 @@
 class GoodHabit < ApplicationRecord
-  validates :
+
 end
--- spec/models/good_habit_spec.rb
+++ spec/models/good_habit_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe GoodHabit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "習慣名と効果、作成したユーザIDを入力すれば、モデルが有効になること" do
+  end
+
+  it "習慣名とユーザーIDの組が重複していると、モデルが無効になること" do
+  end
+
+  it "習慣名がなければ、モデルが無効になること" do
+  end
+  it "習慣名がスペースだけならば、モデルが無効になること" do
+  end
+
+  it "効果がなければ、モデルが無効になること" do
+  end
+  it "効果がスペースだけならば、モデルが無効になること" do
+  end
+  it "効果の文字数が400文字を超えると、モデルが無効になること" do
+  end
+
+  it "作成したユーザーIDがなければ、モデルが無効になること" do
+  end
+  it "作成したユーザーIDがスペースだけならば、モデルが無効になること" do
+  end
+
+  it "効果の文字数が400文字を超えると、モデルが無効になること" do
+  end
 end
