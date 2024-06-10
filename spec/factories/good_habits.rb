01e99f46919d2aa8c382efd094c31705fca65fb6
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-10 10:24:33 +0900

    効果に関するものを削除する

--- spec/factories/good_habits.rb
+++ spec/factories/good_habits.rb
@@ -2,16 +2,13 @@ FactoryBot.define do
   TEXT_REP_NUM = 10
   factory :good_habit do
     name { "good_habit" }
-    effect { "text" * TEXT_REP_NUM }
     creating_user_id { 1 }
     # association :user
     factory :dup_name_good_habit do
       creating_user_id { 2 }
-      effect { "dup" * TEXT_REP_NUM }
     end
     factory :dup_creating_id_good_habit do
       name { "habit" }
-      effect { "habit_habit" * TEXT_REP_NUM }
     end
   end
 end
--- spec/models/good_habit_spec.rb
+++ spec/models/good_habit_spec.rb
@@ -13,29 +13,20 @@ RSpec.describe GoodHabit, type: :model do
     habit.valid?
     expect(habit.errors[:name]).to include("has already been taken")
   end
-    it "作成したユーザIDが重複していると、モデルが無効になること" do
-    FactoryBot.create(:dup_creating_id_good_habit)
-    habit = FactoryBot.build(:good_habit)
-    habit.valid?
-    expect(habit.errors[:creating_user_id]).to include("has already been taken")
-  end
+
   it "習慣名がなければ、モデルが無効になること" do
     expect(FactoryBot.build(:good_habit, name: nil)).to be_invalid
   end
   it "習慣名がスペースだけならば、モデルが無効になること" do
     expect(FactoryBot.build(:good_habit, name: " ")).to be_invalid
   end
-
-  it "効果がなければ、モデルが無効になること" do
-    expect(FactoryBot.build(:good_habit, effect: nil)).to be_invalid
+  
+  it "作成したユーザIDが重複していると、モデルが無効になること" do
+    FactoryBot.create(:dup_creating_id_good_habit)
+    habit = FactoryBot.build(:good_habit)
+    habit.valid?
+    expect(habit.errors[:creating_user_id]).to include("has already been taken")
   end
-  it "効果がスペースだけならば、モデルが無効になること" do
-    expect(FactoryBot.build(:good_habit, effect: " ")).to be_invalid
-  end
-  it "効果の文字数が400文字を超えると、モデルが無効になること" do
-    expect(FactoryBot.build(:good_habit, effect: "1" * (text_max + 1))).to be_invalid
-  end
-
   it "作成したユーザーIDがなければ、モデルが無効になること" do
     expect(FactoryBot.build(:good_habit, creating_user_id: nil)).to be_invalid
   end
