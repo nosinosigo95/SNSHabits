1dff94afbaa5a9f2ca7016e7d6ee67758a3bf6bf
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-11 18:16:31 +0900

    habitsテーブルにカラムを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_11_045649) do
+ActiveRecord::Schema.define(version: 2024_06_11_083846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,11 +54,14 @@ ActiveRecord::Schema.define(version: 2024_06_11_045649) do
   create_table "habits", force: :cascade do |t|
     t.string "name"
     t.text "scheme"
-    t.string "doing_time"
+    t.string "working_time"
     t.string "period_for_effect"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.date "recently_viewed_time"
+    t.boolean "challenge"
+    t.boolean "commit"
     t.index ["user_id"], name: "index_habits_on_user_id"
   end
 
