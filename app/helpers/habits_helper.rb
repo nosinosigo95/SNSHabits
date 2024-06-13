176a4f651c13dba54b31233a7b426e877378a2f3
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-13 13:42:38 +0900

    showページを編集する

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -2,4 +2,13 @@ module HabitsHelper
   def get_url_max
     3
   end
+  def get_circumstance(habit)
+    if habit.nil?
+      nil
+    elsif habit.commit?
+      "結果済み"
+    elsif habit.challenge?
+      "挑戦中"
+    end
+  end
 end
