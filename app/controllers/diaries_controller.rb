3903c2ff02873545fe0bb7453e735132cba1fd22
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-17 22:35:02 +0900

    editとupdateアクションを制作する

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -4,9 +4,9 @@ class DiariesController < ApplicationController
     @diary.habit_id = params[:habit_id]
   end
 
-  def create
-    @diary = Diary.create(dairy_params)
-    if(@diary.save)
+  def create 
+    diary = Diary.create(diary_params)
+    if diary.save
       redirect_to root_url
     else
       render :new
@@ -14,16 +14,27 @@ class DiariesController < ApplicationController
   end
 
   def edit
+    @diary = Diary.find(params[:id])
+  end
+
+  def update
+    diary = Diary.find(params[:id])
+    if diary.update(diary_params)
+      redirect_to root_url
+    else
+      render :edit
+    end
   end
 
   def show
+
   end
 
   def destroy
   end
 
   private
-    def dairy_params
+    def diary_params
       params.require(:diary).permit(:description, :doing_time, :private, :action_date).merge(user_id: current_user.id)
     end
 end
