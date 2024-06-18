a8a2b77fe3e31f436e563279729273bdb1f3bd0f
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-18 21:32:19 +0900

    ログインしないと、遷移できないページを決める

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,4 +1,5 @@
 class FollowsController < ApplicationController
+  before_action :authenticate_user!
   def create
     followed_user = User.find(params[:user_id])
     current_user.follow(followed_user)
