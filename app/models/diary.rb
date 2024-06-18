a7f254ac6c4895da77461114cccbbd8a8047d922
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-18 15:06:13 +0900

    エラーメッセージを追加する

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -6,5 +6,5 @@ class Diary < ApplicationRecord
   validates :action_date, presence: true
   validates :description, presence: true, length: {maximum: description_max}
   validates :doing_time, presence: true, format: {with: /\A\d{1,2}:\d{1,2}\z/, message: "に時:分を入力してください"}
-  validates :private, inclusion: {in: [true, false]}
+  validates :private, inclusion: {in: [true, false], message: "選択してください"}
 end
