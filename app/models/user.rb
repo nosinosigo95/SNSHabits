72df9a45234c67b12580b6263eaff38d1999fb1c
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-18 14:15:20 +0900

    確認メールを送らない

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   INTRO_MAX = 400
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable # , :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true, uniqueness: true
   validates :introduction, length: { maximum: INTRO_MAX }
   has_one_attached :icon
