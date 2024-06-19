ceb781b04627ffa0cf840ca71255919d67f209f3
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-19 22:22:32 +0900

    選んだ習慣の日記作成を表示する

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -28,6 +28,11 @@ class DiariesController < ApplicationController
   end
 
   def index
+    favorite_id = /\A[0-9]+\z/.match(params[:search][:favorite_id])
+    if favorite_id[0].present?
+      redirect_to new_diary_url(habit_id: favorite_id[0])
+    end
+
     habit_id = params[:habit_id]
     if habit_id.present?
       @diaries = current_user.diaries.where(habit_id: habit_id)
