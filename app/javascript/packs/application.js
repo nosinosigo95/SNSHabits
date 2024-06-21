9d00ee3e32920c09cb24f7a6019924f26d03f8cb
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-21 18:34:48 +0900

    bodyの余白を削除する

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,7 @@
-import "../stylesheets/application.scss";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
-
-
 
 Rails.start()
 Turbolinks.start()
