2443270191960b2c02ba81b00cdf418ae3ce20bb
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-06-25 21:40:04 +0900

    effectモデルのdependentを削除する

--- app/models/effect.rb
+++ app/models/effect.rb
@@ -1,5 +1,5 @@
 class Effect < ApplicationRecord
-  has_many :effect_habits, dependent: :destroy
+  has_many :effect_habits
   has_many :habits, through: :effect_habits
   validates :effect_item, presence: true, uniqueness: true
 end
