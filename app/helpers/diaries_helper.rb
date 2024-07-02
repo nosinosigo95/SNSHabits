cf13096cae6b552df878074fbc496afd080d1704
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-07-02 11:19:32 +0900

    実行時間のデータを修正する

--- app/helpers/diaries_helper.rb
+++ app/helpers/diaries_helper.rb
@@ -28,7 +28,8 @@ module DiariesHelper
   # [0日前, 1日前, 2日前, 3日前, 4日前, 5日前, 6日前]
   def doing_times_before_days(diaries)
     day_week = 7
-    doing_times = [0] * day_week
+    recently_doing_times_hours = [0.0] * day_week
+    recently_doing_times_minutes = [0.0] * day_week
     before_days = [
       0.day.ago.strftime('%Y-%m-%d'),
       1.day.ago.strftime('%Y-%m-%d'),
@@ -49,17 +50,37 @@ module DiariesHelper
       end
       break if before_days_i >= before_days.count
 
-      doing_times[before_days_i] =
-        doing_times[before_days_i].to_f + change_doing_time_to_hour(diary.doing_time)
+      recently_doing_times_hours[before_days_i] += choose_hour_of_doing_time(diary.doing_time)
+      recently_doing_times_minutes[before_days_i] += choose_minute_of_doing_time(diary.doing_time)
+    end
+    sum_doing_times_per_day(recently_doing_times_hours, recently_doing_times_minutes)
+  end
+
+  def sum_doing_times_per_day(hours, minutes)
+    day_week = 7
+    doing_times = [0.0] * day_week
+    i = 0
+    hours.zip(minutes) do |hour, minute|
+      doing_times[i] +=  (hour + minute / 60.to_f)
+      i += 1
     end
     doing_times
   end
 
   # doing_time: "時：分"
+  def choose_hour_of_doing_time(doing_time)
+    doing_time_match = /\A(\d{1,2}):\d{1,2}\z/.match(doing_time)
+    doing_time_match[1].to_f
+  end
+  def choose_minute_of_doing_time(doing_time)
+    doing_time_match = /\A\d{1,2}:(\d{1,2})\z/.match(doing_time)
+    doing_time_match[1].to_f
+  end
+
   def change_doing_time_to_hour(doing_time)
     doing_time_match = /\A(\d{1,2}):(\d{1,2})\z/.match(doing_time)
-    hours = doing_time_match[0].to_i
-    minutes =  doing_time_match[1].to_i
-    hours.to_f + minutes.to_f / 60.to_f
+    hours = doing_time_match[1].to_f
+    minutes =  doing_time_match[2].to_f
+    hours + minutes / 60.to_f
   end
 end
