f0d998b34eac1202bb821efeed9a7152df22ac32
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-07-03 14:59:23 +0900

    カッコの位置をずらす

--- app/controllers/effects_controller.rb
+++ app/controllers/effects_controller.rb
@@ -2,7 +2,6 @@ class EffectsController < ApplicationController
   def show
     effect = Effect.find_by(id: params[:id])
     redirect_to(controller: "habits", action: "index",
-                habit_index_form: { effect_item: effect.effect_item }
-    )
+                habit_index_form: { effect_item: effect.effect_item })
   end
 end
