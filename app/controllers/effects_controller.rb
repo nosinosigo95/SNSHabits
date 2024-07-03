8be00c6b22365074e317e26b39fcec24084b7ac9
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-07-03 14:38:42 +0900

    rubocopエラーを修正する

--- app/controllers/effects_controller.rb
+++ app/controllers/effects_controller.rb
@@ -1,6 +1,7 @@
 class EffectsController < ApplicationController
   def show
     effect = Effect.find_by(id: params[:id])
-    redirect_to controller: "habits", action: "index", habit_index_form: {effect_item: effect.effect_item}
+    redirect_to controller: "habits", action: "index",
+     habit_index_form: { effect_item: effect.effect_item }
   end
 end
