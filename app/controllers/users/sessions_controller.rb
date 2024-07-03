3f986756f7ac33edda9da82f10f6af6d02f7eceb
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-07-03 15:52:42 +0900

    ログイン時の遷移先を変更する

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+
+  def after_sign_in_path_for(resource)
+    habits_url(habit_index_form: {created: '1' })
+  end
+
+end
