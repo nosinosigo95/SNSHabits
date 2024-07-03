c789cdb2e9fd4eef0b59554c73e3b856b56c6c63
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-07-03 16:07:40 +0900

    rubocopエラーを修正する

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-
   protected
 
   def after_sign_up_path_for(resource)
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-
   protected
 
   def after_sign_in_path_for(resource)
-    habits_url(habit_index_form: {created: '1' })
+    habits_url(habit_index_form: { created: '1' })
   end
 end
